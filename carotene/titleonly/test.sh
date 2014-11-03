5c78b7d69c39d893b7e790651ede93c41e8b1369
tskang <tkang@lat-tkang1.cb.careerbuilder.com careerbuilder.com>
2014-11-03 15:41:13 -0500

    calibration

--- carotene/titleonly/test.sh
+++ carotene/titleonly/test.sh
@@ -1,6 +1,10 @@
 # output file: out.csv
 inputdir=/Users/tkang/github/CaroteneClassifier/carotene-classifier/src/src/main/resources/datasets/cascadeindex
 version=v2
-logfile=run7.log
+prefix=run11_master
+logfile=$prefix.log
+csvfile=$prefix.csv
 
 time python titleOnlyChecker.py $inputdir $version > $logfile
+
+mv out.csv $csvfile
