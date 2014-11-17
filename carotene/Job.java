ce921b547e8c50093dca0e7fa2bdeff95b0bc4ec
tskang <tkang@lat-tkang1.cb.careerbuilder.com careerbuilder.com>
2014-11-17 15:47:44 -0500

    Job_constructor

--- carotene/Job.java
+++ carotene/Job.java
@@ -2,14 +2,22 @@
 //package CaroteneClassifier.CaroteneClassifier;
 
 public class Job {
+	private final String jobid;
 	private final String title;
 	private final String description;
-	private final String title_expected;
-	private ArrayList<Integer> onet_socs;
+	private final ArrayList<String> titles_expected;
 	private int soc; // SOC number without "soc" prefix
-	
+	private ArrayList<Integer> onet_socs;
+	private String onet_id;
+
+	// File Name, Original Title, Expected V2.1 title, Comments, ONet SOCs, Description
+	// v2, DIRECTOR OF SUSTAINABILITY, Director of Strategy OR "Director of Sustainability", We don't have ..., [11, 13], pstrongemspan style ...
 	public Job(String row) {
-	
+		String[] fields = row.split("\t");
+		title = fields[1];
+		title_expected = fields[2];
+		//onet_socs = toSocs(fields[4]);
+		description = fields[5];
 	}
 
 	public Job(String title, String description) {
